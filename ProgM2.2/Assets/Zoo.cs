cc4e98e608e2cef929847cacd729e80140d7cdeb
Elitetykoro <tykoromathijssen@gmail.com>
2024-11-29 10:13:48 +0100

    ProgOpdr1:Abstraction

---
+++ ProgM2.2/Assets/Zoo.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Zoo : MonoBehaviour
+{
+    public List<Animal> animals = new List<Animal>();
+void Start() 
+    {
+        animals.Add(new Dog());
+        animals.Add(new Elephant());
+        animals.Add(new Bird());
+        foreach (Animal a in animals)
+        {
+            a.Move();
+            a.Eat();
+        }
+    }
+}
+public abstract class Animal
+{
+    public abstract void Move();
+    public void Eat() { Debug.Log("Njam Njam"); }
+}
+public class Dog : Animal
+{
+    public override void Move()
+    {
+        Debug.Log("Wiggles its tail while walking");
+    }
+}
+public class Bird : Animal
+{
+    public override void Move()
+    {
+        Debug.Log("Flaps its wings while flying");
+    }
+}
+public class Elephant : Animal
+{
+    public override void Move()
+    {
+        Debug.Log("Stomps around while walking on all fours");
+    }
+}
